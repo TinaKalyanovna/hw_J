f3049db5da6ab0055247bab57412297a60bafadb
Tina <fatina.kalyanovna@gmail.com>
2023-06-21 16:47:29 +0300

    the task #4  from HW #1 was completed

---
+++ HW_1/task_4.java
@@ -0,0 +1,35 @@
+
+
+
+
+
+//Решения на 4 задание к сожалению нет ((
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+package HW_1;
+
+import java.util.Scanner;
+
+public class task_4 {
+     static boolean isRightEquation(String q, String w, String e) {
+        int a = Integer.parseInt(q);
+        int b = Integer.parseInt(w);
+        int c = Integer.parseInt(e);
+        if (a + b == c) {
+            return true;
+        }
+        return false;
+    }
+}
